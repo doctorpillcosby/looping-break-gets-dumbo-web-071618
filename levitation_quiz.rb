21b962287f8c74c5bfaf2c44fd0912becbb121fc
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-07-02 15:12:46 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -2,6 +2,10 @@
 def levitation_quiz
 	loop do
 	  puts "What is the spell that enacts levitation?"
+	  answer = get.chomp
+	  break if answer == "Wingarduim Leviosa"
+	end
+	puts "You passed the Quiz!"
 end
 
 
